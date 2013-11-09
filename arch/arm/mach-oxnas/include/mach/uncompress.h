93004171d75a29b6af4018b69dc38dffc93aa894
Ma Haijun <mahaijuns@gmail.com>
2013-11-09 19:06:05 +0800

    OXNAS: cleanup uncompress.h

--- arch/arm/mach-oxnas/include/mach/uncompress.h
+++ arch/arm/mach-oxnas/include/mach/uncompress.h
@@ -1,4 +1,4 @@
-/* linux/include/asm-arm/arch-oxnas0/uncompress.h
+/* linux/include/asm-arm/arch-oxnas/uncompress.h
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -8,28 +8,16 @@
 #ifndef __ASM_ARCH_UNCOMPRESS_H
 #define __ASM_ARCH_UNCOMPRESS_H
 
-#include <mach/iomap.h>
+#define OXNAS_UART1_BASE 0x44200000
 
 static inline void putc(int c)
 {
-#ifdef CONFIG_OXNAS_UART1
     static volatile unsigned char* uart = (volatile unsigned char*)OXNAS_UART1_BASE;
-#elif defined(CONFIG_OXNAS_UART2)
-    static volatile unsigned char* uart = (volatile unsigned char*)OXNAS_UART2_BASE;
-#elif defined(CONFIG_OXNAS_UART3)
-    static volatile unsigned char* uart = (volatile unsigned char*)OXNAS_UART3_BASE;
-#elif defined(CONFIG_OXNAS_UART4)
-    static volatile unsigned char* uart = (volatile unsigned char*)OXNAS_UART4_BASE;
-#else
-#define NO_UART
-#endif
 
-#ifndef NO_UART
     while (!(uart[5] & 0x20)) { /* LSR reg THR empty bit */
         barrier();
     }
     uart[0] = c;                /* THR register */
-#endif // NO_UART
 }
 
 static inline void flush(void)
